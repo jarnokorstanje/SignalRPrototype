68a56e119b1670694b8c30638a4edbd1e12f3f21
jarnokorstanje <jakorstan@gmail.com>
2022-06-15 13:42:05 +0200

    Remove unnecessary Get method

--- Controllers/MessageController.cs
+++ Controllers/MessageController.cs
@@ -14,21 +14,6 @@ namespace SignalRPrototype.Controllers
             _hubContext = hubContext;
         }
 
-        private static readonly string[] ExampleTexts = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-
-        [HttpGet(Name = "GetMessage")]
-        public Message Get()
-        {
-            return new Message(
-                    ExampleTexts[Random.Shared.Next(ExampleTexts.Length)],
-                    "User 1",
-                    "User 2"
-                );
-        }
-
         [HttpPost(Name = "PostMessage")]
         public async Task<IActionResult> Post(Message message)
         {

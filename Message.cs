eb453542498e9717ef5e7c9ccf5da749e9166291
jarnokorstanje <jakorstan@gmail.com>
2022-06-29 13:14:12 +0200

    Generate message GUID and display response in debug console

--- Message.cs
+++ Message.cs
@@ -1,13 +1,15 @@
 namespace SignalRPrototype;
 public class Message 
 {
+    public Guid guid { get; }
     public string text { get; }
     public string caller { get; }
     public string receiver { get; }
     public DateTime timestamp { get; }
-
+    
     public Message(string text, string caller, string receiver)
     {
+        guid = Guid.NewGuid();
         this.text = text;
         this.caller = caller;
         this.receiver = receiver;
--- MessageHub.cs
+++ MessageHub.cs
@@ -13,7 +13,7 @@ public class MessageHub : Hub
         return Clients.Group(groupName).SendAsync("message", message);
     }
 
-    public async Task AddToGroup(string username)
+    public async Task<Task> AddToGroup(string username)
     {
         var groupName = "user_" + username;
 
@@ -21,6 +21,14 @@ public class MessageHub : Hub
 
         var message = new Message($"{Context.ConnectionId} has joined the group {groupName}.", "MessageHub", groupName);
 
-        await Clients.Group(groupName).SendAsync("message", message);
+        return Clients.Group(groupName).SendAsync("message", message);
+    }
+
+    
+    public void MessageResponse(Guid guid)
+    {
+        Console.WriteLine("Response message received by server: " + guid);
+
+        // TODO: search and delete message with corresponding GUID
     }
 }
